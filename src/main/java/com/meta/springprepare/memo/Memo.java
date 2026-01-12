8cacbf6b104cb72bd89cf9dd9d4c40decec96c41
yunminjae03 <dbsaslwo012@g.hongik.ac.kr>
2026-01-12 11:47:38 +0900

    chore: Lombok의 기본생성자, 모든인수를 받는 생성자, 필수인수를 받는 생성자

--- src/main/java/com/meta/springprepare/memo/Memo.java
+++ src/main/java/com/meta/springprepare/memo/Memo.java
@@ -1,67 +1,28 @@
-
-
 package com.meta.springprepare.memo;
 
 import java.util.Date;
-import lombok.Generated;
+
+import lombok.*;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 
 public class Memo {
-  private String username;
+  private final String username;
   private String title;
   private String contents;
   private Date createAt;
   private Date updatedAT;
 
+  public Memo(String username, Date updatedAT) {
+    this.username = username;
+    this.updatedAT = updatedAT;
+  }
+
   public void writeMemo() {
   }
 
-  @Generated
-  public String getUsername() {
-    return this.username;
-  }
-
-  @Generated
-  public String getTitle() {
-    return this.title;
-  }
-
-  @Generated
-  public String getContents() {
-    return this.contents;
-  }
-
-  @Generated
-  public Date getCreateAt() {
-    return this.createAt;
-  }
-
-  @Generated
-  public Date getUpdatedAT() {
-    return this.updatedAT;
-  }
-
-  @Generated
-  public void setUsername(final String username) {
-    this.username = username;
-  }
-
-  @Generated
-  public void setTitle(final String title) {
-    this.title = title;
-  }
-
-  @Generated
-  public void setContents(final String contents) {
-    this.contents = contents;
-  }
-
-  @Generated
-  public void setCreateAt(final Date createAt) {
-    this.createAt = createAt;
-  }
-
-  @Generated
-  public void setUpdatedAT(final Date updatedAT) {
-    this.updatedAT = updatedAT;
-  }
 }
